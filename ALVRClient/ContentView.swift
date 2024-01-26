cac697590ec0279cf8b095013a5e3c7025553a54
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2024-01-26 00:26:14 -0500

    Initial Commit

---
+++ ALVRClient/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  ALVRClient
+//
+//  Created by Zhuowei Zhang on 1/26/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    @State private var enlarge = false
+    @State private var showImmersiveSpace = false
+    @State private var immersiveSpaceIsShown = false
+
+    @Environment(\.openImmersiveSpace) var openImmersiveSpace
+    @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
+
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let scene = try? await Entity(named: "Scene", in: realityKitContentBundle) {
+                content.add(scene)
+            }
+        } update: { content in
+            // Update the RealityKit content when SwiftUI state changes
+            if let scene = content.entities.first {
+                let uniformScale: Float = enlarge ? 1.4 : 1.0
+                scene.transform.scale = [uniformScale, uniformScale, uniformScale]
+            }
+        }
+        .onChange(of: showImmersiveSpace) { _, newValue in
+            Task {
+                if newValue {
+                    switch await openImmersiveSpace(id: "ImmersiveSpace") {
+                    case .opened:
+                        immersiveSpaceIsShown = true
+                    case .error, .userCancelled:
+                        fallthrough
+                    @unknown default:
+                        immersiveSpaceIsShown = false
+                        showImmersiveSpace = false
+                    }
+                } else if immersiveSpaceIsShown {
+                    await dismissImmersiveSpace()
+                    immersiveSpaceIsShown = false
+                }
+            }
+        }
+        .gesture(TapGesture().targetedToAnyEntity().onEnded { _ in
+            enlarge.toggle()
+        })
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomOrnament) {
+                VStack (spacing: 12) {
+                    Toggle("Enlarge RealityView Content", isOn: $enlarge)
+                    Toggle("Show ImmersiveSpace", isOn: $showImmersiveSpace)
+                }
+            }
+        }
+    }
+}
+
+#Preview(windowStyle: .volumetric) {
+    ContentView()
+}
