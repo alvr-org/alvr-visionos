e668d6473b03a52af257b43a196514e171971dda
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2024-02-04 02:42:32 -0500

    start integrating video decode into main loop
    doesn't work yet since I'm not sending any poses, so ALVR doesn't render anything

--- ALVRClient/ALVRClientApp.swift
+++ ALVRClient/ALVRClientApp.swift
@@ -44,13 +44,72 @@ struct MetalRendererApp: App {
 }
 #endif
 
-#if false && !os(visionOS)
+#if true && !os(visionOS)
+
+let H264_NAL_TYPE_SPS = 7
+
 @main
 struct Main {
+    static func pollNal() -> (Data, UInt64)? {
+        let nalLength = alvr_poll_nal(nil, nil)
+        if nalLength == 0 {
+            return nil
+        }
+        let nalBuffer = malloc(Int(nalLength))!
+        defer { free(nalBuffer) }
+        var nalTimestamp:UInt64 = 0
+        alvr_poll_nal(nalBuffer, &nalTimestamp)
+        return (Data(bytes: nalBuffer, count: Int(nalLength)), nalTimestamp)
+    }
+    
+    static func createVideoDecoder(initialNals: Data) -> VTDecompressionSession {
+        let nalHeader:[UInt8] = [0x00, 0x00, 0x00, 0x01]
+        var videoFormat:CMFormatDescription? = nil
+        var err:OSStatus = 0
+        
+        // First two are the SPS and PPS
+        // https://source.chromium.org/chromium/chromium/src/+/main:third_party/webrtc/sdk/objc/components/video_codec/nalu_rewriter.cc;l=228;drc=6f86f6af008176e631140e6a80e0a0bca9550143
+        
+        err = initialNals.withUnsafeBytes { (b:UnsafeRawBufferPointer) in
+            let nalOffset0 = b.baseAddress!
+            let nalOffset1 = memmem(nalOffset0 + 4, b.count - 4, nalHeader, nalHeader.count)!
+            let nalLength0 = UnsafeRawPointer(nalOffset1) - nalOffset0 - 4
+            let nalLength1 = b.baseAddress! + b.count - UnsafeRawPointer(nalOffset1) - 4
+
+            let parameterSetPointers = [unsafeBitCast(nalOffset0 + 4, to: UnsafePointer<UInt8>.self), unsafeBitCast(nalOffset1 + 4, to: UnsafePointer<UInt8>.self)]
+            let parameterSetSizes = [nalLength0, nalLength1]
+            return CMVideoFormatDescriptionCreateFromH264ParameterSets(allocator: nil, parameterSetCount: 2, parameterSetPointers: parameterSetPointers, parameterSetSizes: parameterSetSizes, nalUnitHeaderLength: 4, formatDescriptionOut: &videoFormat)
+        }
+
+
+        if err != 0 {
+            fatalError("format?!")
+        }
+        print(videoFormat)
+        
+        let videoDecoderSpecification:[NSString: AnyObject] = [:]
+        let destinationImageBufferAttributes:[NSString: AnyObject] = [:]
+
+        var decompressionSession:VTDecompressionSession? = nil
+        err = VTDecompressionSessionCreate(allocator: nil, formatDescription: videoFormat!, decoderSpecification: videoDecoderSpecification as CFDictionary, imageBufferAttributes: destinationImageBufferAttributes as CFDictionary, outputCallback: nil, decompressionSessionOut: &decompressionSession)
+        if err != 0 {
+            fatalError("format?!")
+        }
+        return decompressionSession!
+    }
+    
+    static func feedVideoIntoDecoder(decompressionSession: VTDecompressionSession, nals: Data, timestamp: UInt64) {
+        // TODO(zhuowei)
+    }
+    
     static func main() {
+        let startTime = mach_absolute_time()
+        var wroteOneFrame = false
+        var vtDecompressionSession:VTDecompressionSession? = nil
         let refreshRates:[Float] = [60]
         alvr_initialize(nil, nil, 1024, 1024, refreshRates, Int32(refreshRates.count), true)
         alvr_resume()
+        alvr_request_idr()
         print("alvr resume!")
         var alvrEvent = AlvrEvent()
         while true {
@@ -66,25 +125,61 @@ struct Main {
                     hudMessageBuffer.deallocate()
                 case ALVR_EVENT_STREAMING_STARTED.rawValue:
                     print("streaming started: \(alvrEvent.STREAMING_STARTED)")
+                    alvr_request_idr()
+                    alvr_send_tracking(mach_absolute_time() - startTime, nil, 0)
                 case ALVR_EVENT_STREAMING_STOPPED.rawValue:
                     print("streaming stopped")
                 case ALVR_EVENT_HAPTICS.rawValue:
                     print("haptics: \(alvrEvent.HAPTICS)")
                 case ALVR_EVENT_CREATE_DECODER.rawValue:
                     print("create decoder: \(alvrEvent.CREATE_DECODER)")
+                    while true {
+                        guard let (nal, timestamp) = pollNal() else {
+                            fatalError("create decoder: failed to poll nal?!")
+                            break
+                        }
+                        print(nal.count, timestamp)
+                        NSLog("%@", nal as NSData)
+                        if nal[4] & 0x1f == H264_NAL_TYPE_SPS {
+                            // here we go!
+                            vtDecompressionSession = createVideoDecoder(initialNals: nal)
+                            try! nal.write(to: URL(fileURLWithPath: "/tmp/initialNals.h264"))
+                            break
+                        }
+                    }
                 case ALVR_EVENT_FRAME_READY.rawValue:
                     print("frame ready")
+                    while true {
+                        guard let (nal, timestamp) = pollNal() else {
+                            break
+                        }
+                        print(nal.count, timestamp)
+                        if !wroteOneFrame {
+                            wroteOneFrame = true
+                            try! nal.write(to: URL(fileURLWithPath: "/tmp/oneFrame.h264"))
+                        }
+                        if let vtDecompressionSession = vtDecompressionSession {
+                            feedVideoIntoDecoder(decompressionSession: vtDecompressionSession, nals: nal, timestamp: timestamp)
+                        }
+                        // TODO(zhuowei): hax
+                        alvr_report_frame_decoded(timestamp)
+                        alvr_report_compositor_start(timestamp)
+                        alvr_report_submit(timestamp, 0)
+                    }
+                    // YOLO?
+                    alvr_send_tracking(mach_absolute_time() - startTime, nil, 0)
                 default:
                     print("what")
                 }
+            } else {
+                usleep(100000)
             }
-            usleep(100000)
         }
     }
 }
 #endif
 
-#if true && !os(visionOS)
+#if false && !os(visionOS)
 @main
 struct Main {
     static var theFrameData:Data!
