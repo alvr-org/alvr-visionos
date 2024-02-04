5a4db32e81cc18ce7f269dba443c5b9cab854f5e
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2024-02-04 01:37:23 -0500

    failed to decode video

--- ALVRClient/ALVRClientApp.swift
+++ ALVRClient/ALVRClientApp.swift
@@ -6,6 +6,7 @@ import SwiftUI
 #if os(visionOS)
 import CompositorServices
 #endif
+import VideoToolbox
 
 #if os(visionOS)
 struct ContentStageConfiguration: CompositorLayerConfiguration {
@@ -43,7 +44,7 @@ struct MetalRendererApp: App {
 }
 #endif
 
-#if !os(visionOS)
+#if false && !os(visionOS)
 @main
 struct Main {
     static func main() {
@@ -79,6 +80,88 @@ struct Main {
             }
             usleep(100000)
         }
+    }
+}
+#endif
+
+#if true && !os(visionOS)
+@main
+struct Main {
+    static var theFrameData:Data!
+    static func main() {
+        let videoData = try! Data(contentsOf: URL(fileURLWithPath: "/Users/zhuowei/Documents/Projects/ALVRClient/recording.2024-02-03.20-50-26.h264"))
+        let nalHeader:[UInt8] = [0x00, 0x00, 0x00, 0x01]
+        let nalHeaderShort:[UInt8] = [0x00, 0x00, 0x01]
+        let firstTwoNalsAndRest:[Data] = videoData.withUnsafeBytes { (b:UnsafeRawBufferPointer) in
+            let nalOffset0 = b.baseAddress!
+            let nalOffset1 = memmem(nalOffset0 + 4, b.count - 4, nalHeader, nalHeader.count)!
+            let nalLength0 = UnsafeRawPointer(nalOffset1) - nalOffset0
+            let nalOffset2 = memmem(nalOffset1 + 4, b.count - (nalLength0 + 4), nalHeaderShort, nalHeaderShort.count)!
+            let nalLength1 = UnsafeRawPointer(nalOffset2) - UnsafeRawPointer(nalOffset1)
+            
+            // TODO(zhuowei): lol fix this
+            
+            let nalOffset3 = memmem(nalOffset2 + 4, b.count - (nalLength0 + nalLength1 + 4), nalHeader, nalHeader.count)!
+            let nalLength2 = UnsafeRawPointer(nalOffset3) - UnsafeRawPointer(nalOffset2)
+            return [Data(bytes: nalOffset0, count: nalLength0), Data(bytes: nalOffset1, count: nalLength1), Data(bytes: nalOffset2, count: nalLength2)]
+        }
+        
+        // First two are the SPS and PPS
+        // https://source.chromium.org/chromium/chromium/src/+/main:third_party/webrtc/sdk/objc/components/video_codec/nalu_rewriter.cc;l=228;drc=6f86f6af008176e631140e6a80e0a0bca9550143
+        
+        var videoFormat:CMFormatDescription? = nil
+        var err:OSStatus = 0
+        err = firstTwoNalsAndRest[0].withUnsafeBytes { a in
+            firstTwoNalsAndRest[1].withUnsafeBytes { b in
+                let parameterSetPointers = [unsafeBitCast(a.baseAddress! + 4, to: UnsafePointer<UInt8>.self), unsafeBitCast(b.baseAddress! + 4, to: UnsafePointer<UInt8>.self)]
+                let parameterSetSizes = [a.count - 4, b.count - 4]
+                return CMVideoFormatDescriptionCreateFromH264ParameterSets(allocator: nil, parameterSetCount: 2, parameterSetPointers: parameterSetPointers, parameterSetSizes: parameterSetSizes, nalUnitHeaderLength: 4, formatDescriptionOut: &videoFormat)
+            }
+        }
+
+
+        if err != 0 {
+            fatalError("format?!")
+        }
+        print(videoFormat)
+        
+        let videoDecoderSpecification:[NSString: AnyObject] = [:]
+        let destinationImageBufferAttributes:[NSString: AnyObject] = [:]
+
+        var decompressionSession:VTDecompressionSession? = nil
+        err = VTDecompressionSessionCreate(allocator: nil, formatDescription: videoFormat!, decoderSpecification: videoDecoderSpecification as CFDictionary, imageBufferAttributes: destinationImageBufferAttributes as CFDictionary, outputCallback: nil, decompressionSessionOut: &decompressionSession)
+        if err != 0 {
+            fatalError("format?!")
+        }
+        print(decompressionSession)
+        
+        guard let decompressionSession = decompressionSession else {
+            fatalError("fail")
+        }
+        
+        var sampleBuffer:CMSampleBuffer? = nil
+        theFrameData = firstTwoNalsAndRest[2]
+        let nalLen = theFrameData.count - 4
+        theFrameData[0] = UInt8((nalLen >> 24) & 0xff);
+        theFrameData[1] = UInt8((nalLen >> 16) & 0xff);
+        theFrameData[2] = UInt8((nalLen >> 8) & 0xff);
+        theFrameData[3] = UInt8(nalLen & 0xff);
+
+        theFrameData!.withUnsafeBytes {
+            let contiguousBuffer1 = try! CMBlockBuffer(buffer: UnsafeMutableRawBufferPointer(mutating: $0))
+            var contiguousBuffer:CMBlockBuffer! = nil
+            CMBlockBufferCreateWithBufferReference(allocator: nil, referenceBuffer: contiguousBuffer1, offsetToData: 4, dataLength: $0.count - 4, flags: 0, blockBufferOut: &contiguousBuffer)
+            print(contiguousBuffer)
+            
+            err = CMSampleBufferCreate(allocator: nil, dataBuffer: contiguousBuffer, dataReady: true, makeDataReadyCallback: nil, refcon: nil, formatDescription: videoFormat, sampleCount: 1, sampleTimingEntryCount: 0, sampleTimingArray: nil, sampleSizeEntryCount: 0, sampleSizeArray: nil, sampleBufferOut: &sampleBuffer)
+        }
+        guard let sampleBuffer = sampleBuffer else {
+            fatalError("sampleBuffer")
+        }
+        VTDecompressionSessionDecodeFrame(decompressionSession, sampleBuffer: sampleBuffer, flags: ._EnableAsynchronousDecompression, infoFlagsOut: nil) { (status: OSStatus, infoFlags: VTDecodeInfoFlags, imageBuffer: CVImageBuffer?, taggedBuffers: [CMTaggedBuffer]?, presentationTimeStamp: CMTime, presentationDuration: CMTime) in
+            print(status, infoFlags, imageBuffer, taggedBuffers, presentationTimeStamp, presentationDuration)
+        }
+        CFRunLoopRun()
     }
 }
 #endif
