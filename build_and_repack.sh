a8f8711ca4352fd223dff793717173875e05459d
shinyquagsire23 <mtinc2@gmail.com>
2024-09-12 15:33:04 -0600

    Fixup build script

--- build_and_repack.sh
+++ build_and_repack.sh
@@ -19,11 +19,11 @@ rustup target add aarch64-apple-ios
 
 rm -rf ALVR/target
 rm -rf ALVR/build
+rm -rf ALVR/alvr/server
 
-CARGO_TARGET_DIR=ALVR/target cargo build --manifest-path ALVR/Cargo.toml --target=aarch64-apple-ios -p alvr_client_core --profile distribution
-mkdir -p ALVR/build
-cd ALVR/alvr/client_core
-cbindgen --config cbindgen.toml --crate alvr_client_core --output ../../build/alvr_client_core.h
-cd ../../../
-
+CARGO_TARGET_DIR=ALVR/target cargo build --manifest-path ALVR/Cargo.toml --target=aarch64-apple-ios -p alvr_client_core --profile distribution && \
+mkdir -p ALVR/build && \
+cd ALVR/alvr/client_core && \
+cbindgen --config cbindgen.toml --crate alvr_client_core --output ../../build/alvr_client_core.h && \
+cd ../../../ && \
 sh repack_alvr_client.sh
